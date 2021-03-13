c3b4f313ef2923aef28e5f63def7d089f8e65579
hariprasathp <hari.harithunder391@gmail.com>
2021-03-13 22:37:17 +0530

    Resume builder page UI changes.

--- src/containers/ResumeBuilder.tsx
+++ src/containers/ResumeBuilder.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { Navbar, Container, Row, Col } from "react-bootstrap";
+import { Sidebar } from "./Sidebar/Sidebar";
+import { Sheet } from "./Sheet/Sheet";
 
 export const ResumeBuilder: React.FC = () => {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Navbar bg="dark" style={{ height: "7vh" }}>
+        <h3 style={{ color: "white" }}>Resume Builder</h3>
+      </Navbar>
+      <Container
+        fluid
+        style={{
+          backgroundColor: "rgba(150,150,150,0.1)",
+          padding: "10px 0px",
+        }}
+      >
+        <Row>
+          <Col xs={6} md={4}>
+            <Sidebar />
+          </Col>
+          <Col xs={12} md={8} style={{ left: "5%" }}>
+            <Sheet />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
