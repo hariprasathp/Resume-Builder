d30084a71991baacd2ea54b3188577da1aec4173
hariprasathp <hari.harithunder391@gmail.com>
2021-03-13 10:22:33 +0530

    Basic project setup.

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ResumeBuilder } from "./containers/ResumeBuilder";
+
+ReactDOM.render(<ResumeBuilder />, document.getElementById("root"));
