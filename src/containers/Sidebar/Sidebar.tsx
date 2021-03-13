377454b29aa8aa1f2bafd31a08e816150222c7c2
hariprasathp <hari.harithunder391@gmail.com>
2021-03-13 22:37:47 +0530

    Design UI for sidebar.

--- src/containers/Sidebar/Sidebar.tsx
+++ src/containers/Sidebar/Sidebar.tsx
@@ -1,1 +1,50 @@
 import React from "react";
+import { Card, Nav } from "react-bootstrap";
+import { Router, Route, Redirect } from "react-router-dom";
+import Switch from "react-bootstrap/esm/Switch";
+import {
+  ExperienceComponent,
+  EducationComponent,
+  SkillsComponent,
+  HeaderComponent,
+} from "../../components";
+import { createBrowserHistory } from "history";
+
+export const Sidebar: React.FC = () => {
+  const customHistory = createBrowserHistory();
+
+  return (
+    <>
+      <Router history={customHistory}>
+        <Card style={{ position: "sticky", top: 2 }}>
+          <Nav
+            variant="pills"
+            defaultActiveKey="/home"
+            style={{ justifyContent: "space-evenly" }}
+          >
+            <Nav.Item>
+              <Nav.Link href="/header">HEADER</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/experience">EXPERIENCE</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/education">EDUCATION</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/skills">SKILLS</Nav.Link>
+            </Nav.Item>
+          </Nav>
+
+          <Switch>
+            <Route exact path="/" component={HeaderComponent} />
+            <Route exact path="/header" component={HeaderComponent} />
+            <Route path="/experience" component={ExperienceComponent} />
+            <Route path="/education" component={EducationComponent} />
+            <Route path="/skills" component={SkillsComponent} />
+          </Switch>
+        </Card>
+      </Router>
+    </>
+  );
+};
