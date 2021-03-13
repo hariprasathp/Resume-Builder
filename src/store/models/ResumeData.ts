3c28766dd382c4745a77771283f100e612172341
hariprasathp <hari.harithunder391@gmail.com>
2021-03-13 22:39:01 +0530

    Created model from resume data.

---
+++ src/store/models/ResumeData.ts
@@ -0,0 +1,20 @@
+export interface ResumeData {
+  name: string;
+  mobile: number;
+  email: string;
+  address: string;
+  city: string;
+  state: string;
+}
+
+export interface Experience {
+  company: string;
+  designation: string;
+  year: string;
+}
+
+export interface Education {
+  degree: string;
+  year: string;
+  institute: string;
+}
